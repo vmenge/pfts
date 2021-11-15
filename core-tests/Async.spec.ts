24ce39011a9d5df07402818fdb4f3016eae18d90
vmenge <vfmenge@gmail.com>
2021-11-15 21:38:38 +0100

    test: async

---
+++ core-tests/Async.spec.ts
@@ -0,0 +1,370 @@
+import { async, Async, list, some, Option, ok, err, Result } from "../core/src";
+
+describe("Async", () => {
+  describe("Async.create()", () => {
+    it("Creates an Async<number> from 5", done => {
+      const val = Async.create(5);
+
+      expect(val).toBeInstanceOf(Async);
+
+      val.iter(x => {
+        expect(x).toEqual(5);
+        done();
+      });
+    });
+
+    it("Creates an Async<number> from Promise<number>", done => {
+      const val = Async.create(Promise.resolve(5));
+
+      expect(val).toBeInstanceOf(Async);
+
+      val.iter(x => {
+        expect(x).toEqual(5);
+        done();
+      });
+    });
+  });
+
+  describe("Async.ofPromise()", () => {
+    it("Creates an Async<string> from Promise<string>", done => {
+      const val = Async.ofPromise(Promise.resolve("bla"));
+
+      val.iter(x => {
+        expect(x).toEqual("bla");
+        done();
+      });
+    });
+  });
+
+  describe(".toPromise()", () => {
+    it("Returns the inner Promise<number> inside an Async<number>", done => {
+      const val = async(5).toPromise();
+
+      expect(val).toBeInstanceOf(Promise);
+
+      val.then(x => {
+        expect(x).toEqual(5);
+        done();
+      });
+    });
+  });
+
+  describe("Async.map()", () => {
+    it("Executes code against the Async<number> value once the async operation completes.", done => {
+      Async.map((x: number) => x * 2)(async(10)).iter(x => {
+        expect(x).toEqual(20);
+        done();
+      });
+    });
+  });
+
+  describe(".map()", () => {
+    it("Executes code against the Async<number> value once the async operation completes.", done => {
+      async(10)
+        .map(x => x * 2)
+        .iter(x => {
+          expect(x).toEqual(20);
+          done();
+        });
+    });
+  });
+
+  describe("Async.map2()", () => {
+    it("Executes code against both Async<number> values once their operations are completed", done => {
+      Async.map2((x: number, y: number) => x + y)(async(10))(async(5)).iter(x => {
+        expect(x).toEqual(15);
+        done();
+      });
+    });
+
+    it("Executes code against both Async<number> and Promise<number> values once their operations are completed", done => {
+      Async.map2((x: number, y: number) => x + y)(Promise.resolve(10))(async(5)).iter(x => {
+        expect(x).toEqual(15);
+        done();
+      });
+    });
+  });
+
+  describe(".map2()", () => {
+    it("Executes code against both Async<number> values once their operations are completed", done => {
+      async(10)
+        .map2(async(5), (x, y) => x + y)
+        .iter(x => {
+          expect(x).toEqual(15);
+          done();
+        });
+    });
+
+    it("Executes code against both Async<number> and Promise<number> values once their operations are completed", done => {
+      async(10)
+        .map2(Promise.resolve(5), (x, y) => x + y)
+        .iter(x => {
+          expect(x).toEqual(15);
+          done();
+        });
+    });
+  });
+
+  describe("Async.bind()", () => {
+    it("Executes the binder function returning the exepected value", done => {
+      Async.bind((x: number) => async(x + 50))(async(10)).iter(x => {
+        expect(x).toEqual(60);
+        done();
+      });
+    });
+  });
+
+  describe(".bind()", () => {
+    it("Executes the binder function returning the exepected value", done => {
+      async(10)
+        .bind(x => async(x + 50))
+        .iter(x => {
+          expect(x).toEqual(60);
+          done();
+        });
+    });
+  });
+
+  describe(".tee()", () => {
+    it("Returns the same instance of Async<A> after executing a side-effectful method", done => {
+      async(666)
+        .tee(x => console.log(`.tee() called with value ${x}`))
+        .iter(x => {
+          expect(x).toEqual(666);
+          done();
+        });
+    });
+  });
+
+  describe(".zip()", () => {
+    it("Tuples together the result of two Async values", done => {
+      async(10)
+        .zip(async("hello"))
+        .iter(([x, y]) => {
+          expect(x).toEqual(10);
+          expect(y).toEqual("hello");
+          done();
+        });
+    });
+
+    it("Tuples together the result of an Async value and a Promise", done => {
+      async(10)
+        .zip(Promise.resolve("hello"))
+        .iter(([x, y]) => {
+          expect(x).toEqual(10);
+          expect(y).toEqual("hello");
+          done();
+        });
+    });
+  });
+
+  describe(".zip3()", () => {
+    it("Tuples together the result of three Async values", done => {
+      async(10)
+        .zip3(async("hello"), async(false))
+        .iter(([x, y, z]) => {
+          expect(x).toEqual(10);
+          expect(y).toEqual("hello");
+          expect(z).toEqual(false);
+
+          done();
+        });
+    });
+  });
+
+  describe(".and()", () => {
+    it("Adds the result of the given Async<T> value to the tuple wrapped inside a Async instance", done => {
+      async([1, 2])
+        .and(async("three"))
+        .iter(([x, y, z]) => {
+          expect(x).toEqual(1);
+          expect(y).toEqual(2);
+          expect(z).toEqual("three");
+
+          done();
+        });
+    });
+
+    it("Adds the result of the given Promise<T> value to the tuple wrapped inside a Async instance", done => {
+      async([1, 2])
+        .and(Promise.resolve("three"))
+        .iter(([x, y, z]) => {
+          expect(x).toEqual(1);
+          expect(y).toEqual(2);
+          expect(z).toEqual("three");
+
+          done();
+        });
+    });
+  });
+
+  describe(".andWith()", () => {
+    it("Adds the resulting Async<T> of the given function to the tuple wrapped inside a Async instance", done => {
+      async(1)
+        .andWith(x => async(x + 1))
+        .andWith(([x, y]) => async(`${x} ${y}`))
+        .iter(([x, y, z]) => {
+          expect(x).toEqual(1);
+          expect(y).toEqual(2);
+          expect(z).toEqual("1 2");
+
+          done();
+        });
+    });
+
+    it("Adds the resulting Promise<T> of the given function to the tuple wrapped inside a Async instance", done => {
+      async(1)
+        .andWith(x => Promise.resolve(x + 1))
+        .andWith(([x, y]) => async(`${x} ${y}`))
+        .iter(([x, y, z]) => {
+          expect(x).toEqual(1);
+          expect(y).toEqual(2);
+          expect(z).toEqual("1 2");
+
+          done();
+        });
+    });
+  });
+
+  describe("Async.sequenceArray()", () => {
+    it("Sequences a Async<number>[]", done => {
+      const asyncArr = [1, 2, 3].map(async);
+      const actual = Async.sequenceArray(asyncArr);
+
+      expect(actual).toBeInstanceOf(Async);
+
+      actual.iter(x => {
+        expect(x).toEqual([1, 2, 3]);
+        done();
+      });
+    });
+
+    it("Sequences a Promise<number>[]", done => {
+      const promiseArr = [1, 2, 3].map(async x => x);
+      const actual = Async.sequenceArray(promiseArr);
+
+      expect(actual).toBeInstanceOf(Async);
+
+      actual.iter(x => {
+        expect(x).toEqual([1, 2, 3]);
+        done();
+      });
+    });
+  });
+
+  describe("Async.sequenceList()", () => {
+    it("Sequences a List<Async<number>>", done => {
+      const asyncList = list(1, 2, 3).map(async);
+      const actual = Async.sequenceList(asyncList);
+
+      expect(actual).toBeInstanceOf(Async);
+
+      actual.iter(x => {
+        const res = list(1, 2, 3).forall(y => x.contains(y));
+        expect(res).toEqual(true);
+
+        done();
+      });
+    });
+
+    it("Sequences a List<Promise<number>>", done => {
+      const promiseList = list(1, 2, 3).map(async x => x);
+      const actual = Async.sequenceList(promiseList);
+
+      expect(actual).toBeInstanceOf(Async);
+
+      actual.iter(x => {
+        const res = list(1, 2, 3).forall(y => x.contains(y));
+        expect(res).toEqual(true);
+
+        done();
+      });
+    });
+  });
+
+  describe("Async.sequenceOption()", () => {
+    it("Sequences an Option<Async<number>>", done => {
+      const optAsync = some(async(5));
+      const asyncOpt = Async.sequenceOption(optAsync);
+
+      expect(asyncOpt).toBeInstanceOf(Async);
+      asyncOpt.iter(x => {
+        expect(x).toBeInstanceOf(Option);
+        expect(x.raw).toEqual(5);
+
+        done();
+      });
+    });
+  });
+
+  describe("Async.sequenceResult()", () => {
+    it("Sequences a Result<Async<number>, _>", done => {
+      const resAsync = ok(async(5));
+      const asyncRes = Async.sequenceResult(resAsync);
+
+      expect(asyncRes).toBeInstanceOf(Async);
+      asyncRes.iter(x => {
+        expect(x).toBeInstanceOf(Result);
+        expect(x.raw).toEqual(5);
+
+        done();
+      });
+    });
+  });
+
+  describe("Async.sequenceErr()", () => {
+    it("Sequences a Result<_, Async<string>>", done => {
+      const resAsync = err(async("oops"));
+      const asyncRes = Async.sequenceErr(resAsync);
+
+      expect(asyncRes).toBeInstanceOf(Async);
+      asyncRes.iter(x => {
+        expect(x).toBeInstanceOf(Result);
+        expect(x.raw).toEqual("oops");
+
+        done();
+      });
+    });
+  });
+
+  describe("Async.flatten()", () => {
+    it("Flattens a Async<Async<nuymber>> into a Async<number>", done => {
+      Async.flatten(async(async(5))).iter(x => {
+        expect(x).toEqual(5);
+        done();
+      });
+    });
+  });
+
+  describe("Async.ce()", () => {
+    it(".let bindings work correctly", done => {
+      const res = Async.ce()
+        .let("a", async(1))
+        .let("b", () => async(2))
+        .let("c", Promise.resolve(3))
+        .let("d", () => Promise.resolve(4))
+        .return(ctx => ctx);
+
+      res.iter(x => {
+        expect(x).toEqual({ a: 1, b: 2, c: 3, d: 4 });
+        done();
+      });
+    });
+
+    it(".do executes side effects", done => {
+      const obj = { value: 0 };
+
+      const res = Async.ce()
+        .do(() => Async.sleep(10))
+        .do(async () => {
+          obj.value = 10;
+        })
+        .ignore();
+
+      res.iter(() => {
+        expect(obj.value).toEqual(10);
+        done();
+      });
+    });
+  });
+});
--- core/src/Async.ts
+++ core/src/Async.ts
@@ -3,7 +3,16 @@ import { list, List } from "./List";
 import { err, ok, Result } from "./Result";
 import { Flatten, NonVoid } from "./type-utils";
 
+/**
+ * `Async<A>` represents a single operation that does not return a value immediately that usually executes asynchronously.
+ * If successful, upon completion the `Async<A>` object will contain the value of type `A`.
+ *
+ * It is essentially a wrapper for a `Promise<A>` with helper methods.
+ */
 export class Async<A> {
+  /**
+   * The inner `Promise<A>` containing the asynchronous operation.
+   */
   public readonly promise: Promise<A>;
 
   private constructor(value: A | Promise<A>) {
@@ -14,18 +23,57 @@ export class Async<A> {
     this.promise = Promise.resolve(value);
   }
 
+  /**
+   * `(A | Promise<A>) -> Async<A>`
+   *
+   * Creates an `Async<A>` from a type `A` or a `Promise<A>`.
+   */
   static create = <A>(a: A | Promise<A>): Async<A> => new Async(a);
+
+  /**
+   * `Promise<A> -> Async<A>`
+   *
+   * Creates an `Async<A>` from a `Promise<A>`.
+   */
   static ofPromise = <A>(a: Promise<A>): Async<A> => new Async(a);
 
+  /**
+   * `() -> Promise<A>`
+   *
+   * @returns the inner promise from the `Async<A>`.
+   */
   toPromise(): Promise<A> {
     return this.promise;
   }
 
+  /**
+   * `(A -> B) -> Async<B>`
+   *
+   * Evaluates the given function against the result of `Async<A>`once the asynchronous operation is completed.
+   * @param fn mapping function.
+   * @returns The resulting value of the mapping function wrapped in an `Async`.
+   */
   map<B>(fn: (a: A) => B): Async<B> {
     return async(this.promise.then(fn));
   }
 
+  /**
+   * `Async<B> -> ((A, B) -> C) -> Async<C>`
+   *
+   * Given another `Async` value, evaluates the given function against the result of `Async<A>` and `Async<B>` once both operations are completed.
+   * @param b a second `Async` value that must also be evaluated.
+   * @param fn mapping function.
+   * @returns The resulting value of the mapping function wrapped in an `Async`.
+   */
   map2<B, C>(b: Async<B>, fn: (a: A, b: B) => C): Async<C>;
+  /**
+   * `Promise<B> -> ((A, B) -> C) -> Async<C>`
+   *
+   * Given a `Promise<B>`, evaluates the given function against the result of `Async<A>` and `Promise<B>` once both operations are completed.
+   * @param b a `Promise` value that must also be evaluated.
+   * @param fn mapping function.
+   * @returns The resulting value of the mapping function wrapped in an `Async`.
+   */
   map2<B, C>(b: Promise<B>, fn: (a: A, b: B) => C): Async<C>;
   map2<B, C>(b: Async<B> | Promise<B>, fn: (a: A, b: B) => C): Async<C> {
     const x = b instanceof Promise ? b : b.promise;
@@ -35,7 +83,21 @@ export class Async<A> {
     return async(result);
   }
 
+  /**
+   * `(A -> Async<B>) -> Async<B>`
+   *
+   * Evaluates the given function against the result of `Async<A>` once the asynchronous operation is completed.
+   * @param fn a binder function.
+   * @returns The resulting value of the binder function.
+   */
   bind<B>(fn: (a: A) => Async<B>): Async<B>;
+  /**
+   * `(A -> Promise<B>) -> Async<B>`
+   *
+   * Evaluates the given function against the result of `Async<A>` once the asynchronous operation is completed.
+   * @param fn a binder function.
+   * @returns The resulting value of the binder function.
+   */
   bind<B>(fn: (a: A) => Promise<B>): Async<B>;
   bind<B>(fn: (a: A) => Async<B> | Promise<B>): Async<B> {
     const result = this.promise.then(a => {
@@ -47,10 +109,24 @@ export class Async<A> {
     return async(result);
   }
 
+  /**
+   * `(A -> ()) -> Async<void>`
+   *
+   * Executes the given function against the result of `Async<A>` once the asynchronous operation is completed.
+   * @param fn a function that typically executes a side effect.
+   * @returns an 'empty' `Async`.
+   */
   iter(fn: (a: A) => void): Async<void> {
     return async(this.promise.then(fn));
   }
 
+  /**
+   * `(A -> ()) -> Async<A>`
+   *
+   * Executes the given function against the result of `Async<A>` once the asynchronous operation is completed.
+   * @param fn a function that typically executes a side effect.
+   * @returns the same `Async<A>` instance.
+   */
   tee(fn: (a: A) => void): Async<A> {
     return this.map(a => {
       fn(a);
@@ -58,23 +134,82 @@ export class Async<A> {
     });
   }
 
+  /**
+   * `Async<B> -> Async<A * B>`
+   *
+   * @returns the tupled result of the two asynchronous operations wrapped in a `Async` instance.
+   */
   zip<B>(b: Async<B>): Async<[A, B]>;
+  /**
+   * `Promise<B> -> Async<A * B>`
+   *
+   * @returns the tupled result of the two asynchronous operations wrapped in a `Async` instance.
+   */
   zip<B>(b: Promise<B>): Async<[A, B]>;
   zip<B>(b: Async<B> | Promise<B>): Async<[A, B]> {
     return this.bind(a => Async.normalize(b).map(b => [a, b]));
   }
 
+  /**
+   * `Async<B> -> Async<C> -> Async<A * B * C>`
+   *
+   * @returns the tupled result of the three asynchronous operations wrapped in a `Async` instance.
+   */
   zip3<B, C>(b: Async<B>, c: Async<C>): Async<[A, B, C]> {
     return this.bind(a => b.bind(b => c.map(c => [a, b, c])));
   }
 
+  /**
+   * `Async<C> -> Async<A1 * A2 * B1 * B2 ... * C>`
+   *
+   * Appends / tuples the given asynchronous value to to the tuple or value wrapped in an `Async`.
+   * @example
+   * const x = async(5)
+   *             .and(async("victor"))
+   *             .and(async(false));
+   *
+   * expect(x).toEqual(async([5, "victor", false]))
+   */
   and<C, T extends [A, C]>(v: Async<C>): Async<Flatten<T>>;
+  /**
+   * `Promise<C> -> Async<A1 * A2 * B1 * B2 ... * C>`
+   *
+   * Appends / tuples the given asynchronous value to to the tuple wrapped in an `Async`.
+   * @example
+   * const x = async(5)
+   *             .and(async("victor"))
+   *             .and(async(false));
+   *
+   * expect(x).toEqual(async([5, "victor", false]))
+   */
   and<C, T extends [A, C]>(v: Promise<C>): Async<Flatten<T>>;
   and<C, T extends [A, C]>(v: Async<C> | Promise<C>): Async<Flatten<T>> {
     return this.zip(Async.normalize(v)).map(x => x.flat() as Flatten<T>);
   }
 
+  /**
+   * `(A1 * A2 * B1 * B2 ... -> Async<C>) -> Async<A1 * A2 * B1 * B2 ... * C>`
+   *
+   * Appends / tuples the asynchronous value from the evaluated function to the tuple wrapped in an `Async`.
+   * @example
+   * const x = async(5)
+   *             .andWith(x => async(x + 10))
+   *             .andWith(([x, y]) => async(x + y));
+   *
+   * expect(x).toEqual(async([5, 15, 20]))
+   */
   andWith<C, T extends [A, C]>(fn: (a: A) => Async<NonVoid<C>>): Async<Flatten<T>>;
+  /**
+   * `(A1 * A2 * B1 * B2 ... -> Promise<C>) -> Async<A1 * A2 * B1 * B2 ... * C>`
+   *
+   * Appends / tuples the asynchronous value from the evaluated function to the tuple wrapped in an `Async`.
+   * @example
+   * const x = async(5)
+   *             .andWith(x => async(x + 10))
+   *             .andWith(([x, y]) => async(x + y));
+   *
+   * expect(x).toEqual(async([5, 15, 20]))
+   */
   andWith<C, T extends [A, C]>(fn: (a: A) => Promise<NonVoid<C>>): Async<Flatten<T>>;
   andWith<C, T extends [A, C]>(fn: (a: A) => Async<NonVoid<C>> | Promise<NonVoid<C>>): Async<Flatten<T>> {
     const res = this.bind(x => Async.normalize(fn(x)));
@@ -82,30 +217,53 @@ export class Async<A> {
     return this.zip(res).map(x => x.flat() as Flatten<T>);
   }
 
+  /**
+   * `(Async<A> | Promise<A>) -> Async<A>`
+   *
+   * Given an `Async<A>` or `Promise<A>` value, always returns an `Async<A>`.
+   */
   static normalize = <A>(a: Async<A> | Promise<A>): Async<A> => (a instanceof Async ? a : async(a));
 
+  /**
+   * `(A -> B) -> Async<A> -> Async<B>`
+   */
   static map =
     <A, B>(fn: (a: A) => B) =>
     (a: Async<A> | Promise<A>): Async<B> =>
       Async.normalize(a).map(fn);
 
+  /**
+   * `((A, B) -> C) -> Async<A> -> Async<B> -> Async<C>`
+   */
   static map2 =
     <A, B, C>(fn: (a: A, b: B) => C) =>
     (a1: Async<A> | Promise<A>) =>
     (a2: Async<B> | Promise<B>): Async<C> =>
       Async.normalize(a1).map2(Async.normalize(a2), fn);
 
+  /**
+   * `(A -> Async<B>) -> Async<A> -> Async<B>`
+   */
   static bind =
     <A, B>(fn: (a: A) => Async<B> | Promise<B>) =>
     (x: Async<A> | Promise<A>): Async<B> =>
       Async.normalize(x).bind(y => Async.normalize(fn(y)));
 
+  /**
+   * `(A -> ()) -> Async<()>`
+   */
   static iter =
     <A>(fn: (a: A) => void) =>
     (x: Async<A> | Promise<A>): Async<void> =>
       Async.normalize(x).iter(fn);
 
+  /**
+   * `Async<A>[] -> Async<A[]>`
+   */
   static sequenceArray<A>(xs: Async<A>[]): Async<A[]>;
+  /**
+   * `Promise<A>[] -> Async<A[]>`
+   */
   static sequenceArray<A>(xs: Promise<A>[]): Async<A[]>;
   static sequenceArray<A>(xs: Async<A>[] | Promise<A>[]): Async<A[]> {
     if (xs.length === 0) return async([]);
@@ -120,7 +278,13 @@ export class Async<A> {
     return async(Promise.all(xs as Promise<A>[]));
   }
 
+  /**
+   * `List<Async<A>> -> Async<List<A>>`
+   */
   static sequenceList<A>(as: List<Async<A>>): Async<List<A>>;
+  /**
+   * `List<Promise<A>> -> Async<List<A>>`
+   */
   static sequenceList<A>(as: List<Promise<A>>): Async<List<A>>;
   static sequenceList<A>(as: List<Async<A>> | List<Promise<A>>): Async<List<A>> {
     const xs = as.toArray();
@@ -137,6 +301,9 @@ export class Async<A> {
     return async(Promise.all(xs as Promise<A>[])).map(List.ofArray);
   }
 
+  /**
+   * `Option<Async<A>> -> Async<Option<A>>`
+   */
   static sequenceOption<A>(oa: Option<Async<A>>): Async<Option<A>> {
     if (oa.isSome) {
       return oa.raw!.map(option);
@@ -145,6 +312,9 @@ export class Async<A> {
     return async(none());
   }
 
+  /**
+   * `Result<Async<A>, B> -> Async<Result<A, B>>`
+   */
   static sequenceResult<A, B>(ra: Result<Async<A>, B>): Async<Result<A, B>> {
     if (ra.isOk) {
       return ra.value.map(ok);
@@ -153,6 +323,9 @@ export class Async<A> {
     return async(err(ra.err));
   }
 
+  /**
+   * `Result<A, Async<B>> -> Async<Result<A, B>`
+   */
   static sequenceErr<A, B>(ra: Result<A, Async<B>>): Async<Result<A, B>> {
     if (ra.isOk) {
       return async(ok(ra.value));
@@ -161,18 +334,44 @@ export class Async<A> {
     return ra.err.map(err);
   }
 
+  /**
+   * `number -> Async<void>`
+   *
+   * @param ms number of miliseconds to wait.
+   */
   static sleep = (ms: number): Async<void> => async(new Promise(resolve => setTimeout(() => resolve(), ms)));
 
+  /**
+   * `Async<Async<A>> -> Async<A>`
+   *
+   * Flattens a nested `Async<A>`.
+   */
   static flatten = <A>(aa: Async<Async<A>>): Async<A> => aa.bind(x => x);
 
   static ce = () => new AsyncComputation(async({}));
 }
 
+/**
+ * `(A | Promise<A>) -> Async<A>`
+ *
+ * Creates an `Async<A>` from a type `A` or a `Promise<A>`.
+ */
 export const async = Async.create;
 
 class AsyncComputation<A extends Object> {
   constructor(private readonly ctx: Async<A>) {}
 
+  /**
+   * Assigns a value to a variable inside the computation's scope.
+   * @example
+   * const res =
+   *   Async.ce()
+   *     .let('x', async(5))
+   *     .let('y', () => async(10))
+   *     .return(({ x, y }) => x + y);
+   *
+   * expect(res).toEqual(async(15));
+   */
   public let<K extends string, T>(
     k: K,
     other: Promise<T> | Async<T> | ((ctx: A) => Promise<T> | Async<T>)
@@ -190,6 +389,17 @@ class AsyncComputation<A extends Object> {
     return new AsyncComputation(ctx as any);
   }
 
+  /**
+   * Executes and awaits a side-effectful operation.
+   * @example
+   * const res =
+   *   Async.ce()
+   *     .let('x', async(5))
+   *     .do(({ x }) => Async.sleep(x)) // will sleep for 5ms
+   *     .return(({ x }) => x);
+   *
+   * expect(res).toEqual(async(5));
+   */
   public do(fn: (ctx: A) => Promise<void> | Async<void> | void): AsyncComputation<A> {
     const ctx = this.ctx.promise.then(ctx => {
       const res = fn(ctx);
@@ -207,10 +417,30 @@ class AsyncComputation<A extends Object> {
     return new AsyncComputation(async(ctx));
   }
 
+  /**
+   * Returns a value from the computation expression.
+   * @example
+   * const res =
+   *   Async.ce()
+   *     .let('x', async(5))
+   *     .let('y', () => async(10))
+   *     .return(({ x, y }) => x + y);
+   *
+   * expect(res).toEqual(async(15));
+   */
   public return<T>(fn: (ctx: A) => T): Async<T> {
     return Async.map(fn)(this.ctx);
   }
 
+  /**
+   * Ignores the value from the computation expression.
+   * @example
+   * const res: Async<void> =
+   *   Async.ce()
+   *     .do(() => Async.sleep(5000))
+   *     .do(() => console.log('I just waited 5s!'))
+   *     .ignore();
+   */
   public ignore(): Async<void> {
     return Async.iter(_ => {})(this.ctx);
   }
